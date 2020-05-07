df809785427d705d7226ef35279e8914d7c4a042
Gabriel Gomes <gabrielcesariogomes@gmail.com>
2020-05-07 20:40:17 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return <h1>Gabriel</h1>;
+}
+
+const container = document.getElementById('root');
+
+container ? ReactDOM.render(<App />, container) : null;
